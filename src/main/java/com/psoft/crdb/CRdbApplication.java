94a1491873e06ebb938fd9ad11f7865c4ece4819
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-10-25 17:23:06 -0300

    project set up

---
+++ src/main/java/com/psoft/crdb/CRdbApplication.java
@@ -0,0 +1,13 @@
+package com.psoft.crdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CRdbApplication.class, args);
+    }
+
+}
